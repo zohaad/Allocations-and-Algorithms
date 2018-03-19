066bea115a2f79866effe51d7d3c1973202e0bae
Zohaad Fazal <zohaad.fazal@gmail.com>
2018-03-19 17:46:47 +0100

    idea of passing scan objects works

--- Reader.java
+++ Reader.java
@@ -1,5 +1,13 @@
 public class Reader {
-	public Reader(int size) {
-		int[][] matrix = new int[size][size];
+
+	public static void main(String[] args) {
+		int[][] matrix = null;
+
+		matrix = new int[2][2];
+		matrix[0][0] = 5;
+		System.out.println(matrix[0][0]);
 	}
+
+
+
 }
--- SteinerTree.java
+++ SteinerTree.java
@@ -1,24 +1,29 @@
 import java.util.*;
 import java.io.*;
 
+
 public class SteinerTree {
 	public static void main(String[] args) {
 		// max int array is size 22516
 		//int[][] cost = new int[22516][22516];
+
+		String filename = "instance001.gr";
+		File file = new File(filename);
 		try {
-			int[][] cost = read_matrix("instance001.gr"); // change to args[0] later
+			Scanner scan = new Scanner(file);
+			int[] nodes_edges = nodes_edges(scan);
+			int[][] cost = read_matrix(scan, nodes_edges[0]);
+
+			System.out.println(cost[1129][1254]);
+			System.out.println(nodes_edges[0] + " " + nodes_edges[1]);
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-
-		//System.out.println(cost[0][0]);
+		
 	}
 
-	public static int[][] read_matrix(String filename)
-	throws java.io.FileNotFoundException {
-
-		File file = new File(filename);
-		Scanner scan = new Scanner(file);
+	public static int[] nodes_edges(Scanner scan) {
 
 		scan.nextLine(); // 1st line is useless
 
@@ -27,28 +32,35 @@ public class SteinerTree {
 
 		line = scan.nextLine();
 		int edges = Integer.parseInt(line.split(" ")[1]);
-		
-		System.out.println(edges);
 
+		int[] nodes_edges = {nodes, edges};
+		return nodes_edges;
+	}
+
+	public static int[][] read_matrix(Scanner scan, int nodes)
+	throws java.io.FileNotFoundException {
 
 		int[][] cost = new int[nodes][nodes];
 		int from;
 		int to;
 		int c;
 		// method for edges
-		line = scan.nextLine();
-		while (line != "END") {
+		String line = scan.nextLine();
+		while (!line.equals("END")) {
 			String[] line_array = line.split(" ");
-			System.out.println(line);
-			from = Integer.parseInt(line_array[1]);
-			from--;
-			to = Integer.parseInt(line_array[2]);
-			to--;
-			c = Integer.parseInt(line_array[3]);
-			line = scan.nextLine();
-			System.out.println(line == "END");
-		}
 
+			from =  Integer.parseInt(line_array[1]) - 1;
+			to = Integer.parseInt(line_array[2]) - 1;
+			c = Integer.parseInt(line_array[3]);
+			cost[from][to] = c;
+			cost[to][from] = c;
+			line = scan.nextLine();
+
+		}
 		return cost;
 	} 
+
+	public static void terminals(Scanner scan) {
+		
+	}
 }
