cda42b4dbb1b7ce7654ba87cfa4921a01455dcf7
Zohaad Fazal <zohaad.fazal@gmail.com>
2018-03-19 16:53:54 +0100

    hi

--- SteinerTree.java
+++ SteinerTree.java
@@ -6,19 +6,49 @@ public class SteinerTree {
 		// max int array is size 22516
 		//int[][] cost = new int[22516][22516];
 		try {
-			read_matrix(args[0]);
+			int[][] cost = read_matrix("instance001.gr"); // change to args[0] later
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+
+		//System.out.println(cost[0][0]);
 	}
 
-	public static void read_matrix(String filename)
+	public static int[][] read_matrix(String filename)
 	throws java.io.FileNotFoundException {
 
 		File file = new File(filename);
 		Scanner scan = new Scanner(file);
 
-		String s = scan.nextLine();
-		System.out.println(s);
+		scan.nextLine(); // 1st line is useless
+
+		String line = scan.nextLine();
+		int nodes = Integer.parseInt(line.split(" ")[1]);
+
+		line = scan.nextLine();
+		int edges = Integer.parseInt(line.split(" ")[1]);
+		
+		System.out.println(edges);
+
+
+		int[][] cost = new int[nodes][nodes];
+		int from;
+		int to;
+		int c;
+		// method for edges
+		line = scan.nextLine();
+		while (line != "END") {
+			String[] line_array = line.split(" ");
+			System.out.println(line);
+			from = Integer.parseInt(line_array[1]);
+			from--;
+			to = Integer.parseInt(line_array[2]);
+			to--;
+			c = Integer.parseInt(line_array[3]);
+			line = scan.nextLine();
+			System.out.println(line == "END");
+		}
+
+		return cost;
 	} 
 }
