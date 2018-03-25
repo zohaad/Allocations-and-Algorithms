af3444a2316ef49689ca292cc00f322a97a9a933
MarkSteijns <37546679+MarkSteijns@users.noreply.github.com>
2018-03-25 09:33:54 +0200

    Add files via upload

--- MST.java
+++ MST.java
@@ -2,81 +2,163 @@ import java.util.*;
 
 public class MST {
 
-	private int[][] cost;
-	private int[]	terminals;
-	private ArrayList<Integer> leaves;
-	private ArrayList<Integer>[] pre; //dit werkt niet denk ik, hoe lossen we dit op?
+    private int[][] cost;
+    private int[][] next;
+    private int[]	terminals;
+    private int     max;
+    private ArrayList<Integer> leaves;
+    private ArrayList<Integer> pre; //dit werkt niet denk ik, hoe lossen we dit op?
 
-	public MST(int[][] cost, int[] terminals) {
-		this.cost      = cost;
-		this.terminals = terminals;
-		this.leaves = new ArrayList<>();
-		// this.pre = new ArrayList[cost.length]; //dit werkt niet denk ik, hoe lossen we dit op?
-	}
+    public MST(int[][] cost, int[] terminals) {
+        this.cost      = cost;
+        this.next      = new int[cost.length][];
+        for (int i=0; i<cost.length; i++) {
+            this.next[i] = new int[i+1];
+            for (int j=0;j<this.next[i].length;j++){
+                if(i!=j) {
+                    next[i][j] = j + 1;
+                }
+            }
+        }
+        this.terminals = terminals;
+        this.max       = (int) (Integer.MAX_VALUE / 2 - 1);
+        this.leaves    = new ArrayList<>();
+        this.pre       = new ArrayList<>(); //dit werkt niet denk ik, hoe lossen we dit op?
+    }
 
-	public void delete_leaves() {
-		int max = (int) (Integer.MAX_VALUE / 2 - 1);
-		
-		// implement while loop that runs until total number of leaves is zero
+    public void delete_leaves() {
+        //int max = (int) (Integer.MAX_VALUE / 2 - 1);
 
-		System.out.println("Beginning leaf deletion");
-		
-		int old_leave_size; 
-		do {
-			// for every vertex v, that is not a terminal
-			for (int v = 0; v < this.cost.length; v++) {
-				// if not a terminal and not a leaf already
-				if (!terminal_check(v) && !this.leaves.contains(v)) {
-					// arraylist with adjacent nodes
-					ArrayList<Integer> adjacent_to_v = new ArrayList<>();
+        // implement while loop that runs until total number of leaves is zero
 
-					for (int i = 0; i < this.cost.length; i++) {
-						if (i == v) continue; // don't check if v is adjacent to itself
-						if (cost(v, i) < max) { // if edge exists 
-							adjacent_to_v.add(i); // add to adjacent arraylist
-						}
-					}
+        System.out.println("Beginning leaf deletion");
 
-					if (adjacent_to_v.size() == 1) { // a leaf only has one adjacent vertex
-						this.leaves.add(v);
-						System.out.println(v);
-					}
-				}
-			}
-			System.out.println("# leaves deleted: " + this.leaves.size());	
-			old_leave_size = this.leaves.size();
+        int old_leave_size;
+        do {
+            // for every vertex v, that is not a terminal
+            for (int v = 0; v < this.cost.length; v++) {
+                // if not a terminal and not a leaf already
+                if (!terminal_check(v) && !this.leaves.contains(v)) {
+                    // arraylist with adjacent nodes
+                    ArrayList<Integer> adjacent_to_v = new ArrayList<>();
 
-		} while (this.leaves.size() != old_leave_size);
-	}
+                    for (int i = 0; i < this.cost.length; i++) {
+                        if (i == v) continue; // don't check if v is adjacent to itself
+                        if (cost(v, i) < this.max) { // if edge exists
+                            adjacent_to_v.add(i); // add to adjacent arraylist
+                        }
+                    }
 
-	// to deal with upper triangle
-	public int cost(int a, int b) {
-		return a > b ? this.cost[a][b] : this.cost[b][a];//a>b return [a][b] else return [b][a]
-	}
-
-	public boolean terminal_check(int v) {
-		for (int i = 0; i < this.terminals.length; i++) {
-			if (this.terminals[i] == v)
-				return true;
-
-		return false;
-	}
-	
-	 //creates a matrix with the shortest path from any a to any b (using the Floyd-Warschall algorithm)
-    public void Floyd_Warschall_Matrix() {
-        for (int k = 0; k < this.cost.length; k++) {
-            for (int i = 0; i < this.cost.length; i++) {
-                for (int j = 0; j < this.cost.length; j++) {
-                    if (cost(i,k) + cost(k,j) < cost(i,j)) {
-                        if(i > j) {this.cost[i][j] = cost(i,k) + cost(k,j);}
-                        else{this.cost[j][i] = cost(i,k) + cost(k,j);}
-                            //this.pre[i].add[j]
+                    if (adjacent_to_v.size() == 1) { // a leaf only has one adjacent vertex
+                        this.leaves.add(v);
+                        System.out.println(v);
                     }
                 }
-
             }
-            System.out.println(k);
+            System.out.println("# leaves deleted: " + this.leaves.size());
+            old_leave_size = this.leaves.size();
+
+        } while (this.leaves.size() != old_leave_size);
+    }
+
+    // to deal with upper triangle
+    public int cost(int a, int b) {
+        return a > b ? this.cost[a][b] : this.cost[b][a]; //a>b return [a][b] else return [b][a]
+    }
+
+    public boolean terminal_check(int v) {
+        for (int i = 0; i < this.terminals.length; i++) {
+            if (this.terminals[i] == v)
+                return true;
+        }
+        return false;
+    }
+
+    //creates a matrix with the shortest path from any a to any b (using the Floyd-Warschall algorithm)
+    public void Floyd_Warschall_Matrix() {
+        //int[][] next = new int [cost.length][cost.length];
+        /*for (int i=0; i<this.cost.length; i++){
+            for (int j=0; j<this.cost[i].length;j++){
+                if(cost(i,j)< this.max){
+                    this.next[i][j] = j+1;
+                }
+            }
+        }*/
+
+
+        for (int k = 0; k < this.cost.length; k++) {
+            for (int i = 0; i < this.cost.length; i++) {
+                for (int j = 0; j < this.cost[i].length; j++) {
+                    if (cost(i,k) + cost(k,j) < this.cost(i,j)) {
+                        /*if(i > j) {this.cost[i][j] = cost(i,k) + cost(k,j);}
+                        else{this.cost[j][i] = cost(i,k) + cost(k,j);}*/
+                        this.cost[i][j] = cost(i,k) + cost(k,j); //it is always the case that i>j
+                        this.next[i][j] = this.next[i][k]; //waarom is this.next niet nodig??
+                    }
+                }
+            }
+            //System.out.println(k);
         }
         System.out.println("FW done");
+
+
     }
+
+    public void print_Pre(){
+        //System.out.println(Arrays.toString(this.pre.toArray()));
+        System.out.println(this.pre.size());
+        System.out.println(this.pre);
+    }
+
+    public void getPath(int i, int j){ //always i > j
+        if (this.next[i][j] == j) {
+            this.pre.add(j);
+            System.out.println("= j:" +j);
+        }
+        else if (this.next[i][j] < j) {
+            this.pre.add(j);
+            System.out.println("< j :" +j);
+            getPath(j-1, this.next[i][j]);
+        }
+        else {
+            this.pre.add(j);
+            System.out.println("> j:" +j);
+            getPath(this.next[i][j]-1, j);
+        }
+
+        /*else{
+            if (this.next[j][i] == j) {
+                this.pre.add(j);
+            }
+            else{
+                getPath(this.next[j][i], j);
+                this.pre.add(j);
+            }
+        }*/
+    }
+
+    public void print_Matrix(){
+        System.out.println("Cost: ");
+        for(int i =0; i< this.cost.length; i++) {
+            System.out.print(i+1 +": ");
+            for (int j=0; j<this.cost[i].length; j++){
+                System.out.print(this.cost[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println("j: 1 2 3 4 5 6");
+        System.out.println("next: ");
+        for(int i =0; i< this.cost.length; i++) {
+            System.out.print(i+1 +": ");
+            for (int j=0; j<this.cost[i].length; j++){
+                if (i==j) {
+                    System.out.println("-");
+                }
+                else System.out.print(this.next[i][j] + " ");
+            }
+            //System.out.println();
+        }
+        System.out.println("j: 1 2 3 4 5 6");
+    }
+
 }
--- SteinerTree.java
+++ SteinerTree.java
@@ -6,118 +6,123 @@ import java.io.*;
 */
 
 public class SteinerTree {
-	public static void main(String[] args) {
-		// max int array is size 22516
-		//int[][] cost = new int[22516][22516];
+    public static void main(String[] args) {
+        // max int array is size 22516
+        //int[][] cost = new int[22516][22516];
 
-		String filename = "instance" + args[0] + ".gr";
-		File file = new File(filename);
-		try {
-			Scanner scan = new Scanner(file);
-			int[] nodes_edges = amount_nodes_edges(scan);
-			int[][] cost = read_matrix(scan, nodes_edges[0]);
-			int[] terminals = read_terminals(scan);
-			// close scanner
-			scan.close();
+        //String filename = "instance" + args[0] + ".gr";
+        String filename = "C:\\Users\\Mark\\Desktop\\heuristic\\public\\instance000.gr";
+        File file = new File(filename);
+        try {
+            Scanner scan = new Scanner(file);
+            int[] nodes_edges = amount_nodes_edges(scan);
+            int[][] cost = read_matrix(scan, nodes_edges[0]);
+            int[] terminals = read_terminals(scan, nodes_edges[0]);
 
-			// System.out.println(cost[1254][1129]);
-			// System.out.println(cost[1253][1129]);
-			// System.out.println(nodes_edges[0] + " " + nodes_edges[1]);
+            // close scanner
+            scan.close();
 
-			MST mst = new MST(cost, terminals);
-			mst.delete_leaves();
+            //System.out.println("cost:" +cost[6][2]);
+            System.out.println("cost:" +cost[5][1]);
+            System.out.println(nodes_edges[0] + " " + nodes_edges[1]);
 
+            MST mst = new MST(cost, terminals);
+            mst.delete_leaves();
+            //System.out.println("FW:");
+            mst.Floyd_Warschall_Matrix();
+            mst.print_Matrix();
+            mst.getPath(5,3);
+            mst.print_Pre();
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-	}
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-	public static int[] amount_nodes_edges(Scanner scan) {
+    }
 
-		scan.nextLine(); 
+    public static int[] amount_nodes_edges(Scanner scan) {
 
-		String line = scan.nextLine();
-		int nodes = Integer.parseInt(line.split(" ")[1]);
+        scan.nextLine();
 
-		line = scan.nextLine();
-		int edges = Integer.parseInt(line.split(" ")[1]);
+        String line = scan.nextLine();
+        int nodes = Integer.parseInt(line.split(" ")[1]);
 
-		int[] nodes_edges = {nodes, edges};
-		return nodes_edges;
-	}
+        line = scan.nextLine();
+        int edges = Integer.parseInt(line.split(" ")[1]);
 
-	public static int[][] read_matrix(Scanner scan, int nodes)
-	throws java.io.FileNotFoundException {
+        int[] nodes_edges = {nodes, edges};
+        return nodes_edges;
+    }
 
-		int[][] cost = new int[nodes][];
+    public static int[][] read_matrix(Scanner scan, int nodes)
+            throws java.io.FileNotFoundException {
 
-		// make triangular matrix
-		for (int i = 0; i < nodes; i++) {
-			cost[i] = new int[i + 1];
-		}
+        int[][] cost = new int[nodes][];
 
-		int col;
-		int row;
-		int c;
+        // make triangular matrix
+        for (int i = 0; i < nodes; i++) {
+            cost[i] = new int[i + 1];
+        }
 
-		// make other edges cost very large
-		int max = (int) (Integer.MAX_VALUE / 2 - 1);
-		for (int i = 0; i < nodes; i++) {
-			for (int j = 0; j < i; j++) {
-				cost[i][j] = max;
-			}
-		}
+        int col;
+        int row;
+        int c;
 
-		// method for edges
-		String line = scan.nextLine();
-		while (!line.equals("END")) {
-			String[] line_array = line.split(" ");
+        // make other edges cost very large
+        int max = (int) (Integer.MAX_VALUE / 2 - 1);
+        for (int i = 0; i < nodes; i++) {
+            for (int j = 0; j < i; j++) {
+                cost[i][j] = max;
+            }
+        }
 
-			col =  Integer.parseInt(line_array[1]) - 1;
-			row = Integer.parseInt(line_array[2]) - 1;
-			c = Integer.parseInt(line_array[3]);
+        // method for edges
+        String line = scan.nextLine();
+        while (!line.equals("END")) {
+            String[] line_array = line.split(" ");
 
-			// to prevent 009 issues, swap if col is bigger
-			if (col > row) {
-				cost[col][row] =c;
-			}
-			else{
-			cost[row][col] = c;
-			}
-			// enable this if complete matrix is needed (instead of triangular)
-			// cost[row][col] = cost[col][row] = c;
-			line = scan.nextLine();
+            col =  Integer.parseInt(line_array[1]) - 1;
+            row = Integer.parseInt(line_array[2]) - 1;
+            c = Integer.parseInt(line_array[3]);
 
-		}
+            // to prevent 009 issues, swap if col is bigger
+            if (col < row) {
+                cost[row][col] = c;
+            }
+            else{
+                cost[col][row] =c;
+            }
+            // enable this if complete matrix is needed (instead of triangular)
+            // cost[row][col] = cost[col][row] = c;
+            line = scan.nextLine();
 
-		return cost;
-	} 
+        }
 
-	public static int[] read_terminals(Scanner scan) {
-		scan.nextLine();
-		scan.nextLine();
-		int amount = Integer.parseInt(scan.nextLine().split(" ")[1]);
+        return cost;
+    }
 
-		int[] terminals = new int[amount];
-		for (int i = 0; i < amount; i++) {
-			terminals[i] = Integer.parseInt(scan.nextLine().split(" ")[1]);
-		}
-		
-		/* terminal boolean array
-        	int[] terminal = new int[nodes+1];
-            	String line = scan.nextLine();
-            	line = scan.nextLine();
-            	line = scan.nextLine();
+    public static int[] read_terminals(Scanner scan, int n) {
+        scan.nextLine();
+        scan.nextLine();
+        int amount = Integer.parseInt(scan.nextLine().split(" ")[1]);
 
-            	while (!line.equals("END")) {
+        int[] terminals = new int[amount];
+        for (int i = 0; i < amount; i++) {
+            terminals[i] = Integer.parseInt(scan.nextLine().split(" ")[1])-1;
+        }
+
+        /* terminal boolean array
+        int[] terminal = new int[nodes+1];
+            String line = scan.nextLine();
+            line = scan.nextLine();
+            line = scan.nextLine();
+
+            while (!line.equals("END")) {
                 String[] line_array = line.split(" ");
                 terminal[Integer.parseInt(line_array[1])]=1;
                 line = scan.nextLine();
-            	}
-         	*/
-		
-		return terminals;
-	}
+            }
+         */
+        return terminals;
+    }
 }
