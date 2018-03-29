0fdc78c56b88bb13e6262b622786837f5a98117e
Zohaad Fazal <zohaad.fazal@gmail.com>
2018-03-29 15:05:45 +0200

    costs equality verified

--- Main.java
+++ Main.java
@@ -17,10 +17,18 @@ public class Main {
 			Mst mst = new Mst(cost);
 			Integer[][] fw = mst.floyd_warshall();
 			
-			// testing
-			ArrayList<Integer> path = mst.path(58, 70);
-			System.out.print("path from " + 58 + " to " + 70 + ": ");
+			// verifying that costs are equal, change x to 58, y to 70
+			int x = 70;
+			int y = 58;
+			ArrayList<Integer> path = mst.path(x, y);
+			System.out.print("from " + x + " to " + y + ": ");
 			Mst.print_ArrayList(path);
+			int c = 0;
+			for (int i = 0; i < path.size() - 1; i++) {
+				c += mst.cost(path.get(i), path.get(i + 1));
+			}
+			System.out.println("cost: " + c);
+
 
 			fw = Leaves.remove(fw, terminals);
 			
